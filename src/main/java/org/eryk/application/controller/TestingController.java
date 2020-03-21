28f6092653bcee2e2ca08776271b512a0b87279a
Eryk Smolaga <eryk201823@gmail.com>
2020-03-21 12:40:18 +0100

    Add testShowingFootballers() to TestingController. Fix FootballerService
    Method loads list of footballers to model and returns "footballers.jsp" (toDo)
    FootballerService had no @Service, so fix it.

--- src/main/java/org/eryk/application/controller/TestingController.java
+++ src/main/java/org/eryk/application/controller/TestingController.java
@@ -1,12 +1,20 @@
 package org.eryk.application.controller;
 
+import org.eryk.application.entity.Footballer;
+import org.eryk.application.services.FootballerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/test")
 public class TestingController {
+
+    @Autowired
+    private FootballerService footballerService;
 
     @RequestMapping("/mvc")
     public String testMVCConfiguration(Model model){
@@ -14,5 +22,14 @@ public class TestingController {
         String message = "Mvc configuration success!";
         model.addAttribute("testMessage",message);
         return "test-page";
+    }
+
+    @RequestMapping("/showFootballers")
+    public String testShowingFootballers(Model model){
+
+        List<Footballer> list = footballerService.getFootballers();
+
+        model.addAttribute("footballersList", list);
+        return "footballers";
     }
 }
--- src/main/java/org/eryk/application/services/FootballerService.java
+++ src/main/java/org/eryk/application/services/FootballerService.java
@@ -4,10 +4,12 @@ import org.eryk.application.dao.FootballerDAO;
 import org.eryk.application.entity.Footballer;
 import org.eryk.application.serviceInterfaces.FootballerServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
 public class FootballerService implements FootballerServiceInterface {
 
     @Autowired
