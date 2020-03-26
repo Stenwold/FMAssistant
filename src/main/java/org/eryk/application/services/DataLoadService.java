0a179929626838cdf983aebec9f9c2244ddaa09b
Eryk Smolaga <eryk201823@gmail.com>
2020-03-26 13:43:14 +0100

    Update DataLoadService
    Both htmlName and header are null at the beginning. Without it, each request caused duplicating content of header so checking of its order was always failing.
    
    Now BufferReader uses InputStreamReader and FileInputStream, because it allows to specify encoding of file to UTF-8, so there are no more Mojibakes.
    
    Add further processing of data if order of columns is valid.
    FootballerCreatorService populates database on the basis of given rows.

--- src/main/java/org/eryk/application/services/DataLoadService.java
+++ src/main/java/org/eryk/application/services/DataLoadService.java
@@ -1,16 +1,21 @@
 package org.eryk.application.services;
 
 import org.eryk.application.serviceInterfaces.DataLoadServiceInterface;
+import org.eryk.application.serviceInterfaces.FootballerCreatorServiceInterface;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 
+@Service
 public class DataLoadService implements DataLoadServiceInterface {
+
+    @Autowired
+    private FootballerCreatorServiceInterface footballerCreatorService;
 
     private String[] ownPlayersTemplate = new String[]{
             "Name","Tea","Best Pos","Fla","Wor","Vis","Pos","OtB","Cnt","Dec","Det","Cmp","Ant","Bra","Agg",
@@ -18,23 +23,29 @@ public class DataLoadService implements DataLoadServiceInterface {
             "Nat","Pac","Sta","Str"
     };
     private ArrayList<String> header = new ArrayList<>();
-    private String htmlName;
+    private String htmlName = null;
+    private String fileType;
 
     @Override
+
     public void loadData(String htmlName, String fileType){
         if(fileType.equals("ownPlayers")) {
+            this.fileType = fileType;
             this.convert(this.loadFile(htmlName), ownPlayersTemplate);
         }
     }
-
 
     //method load data from html file and returns content as String
 
     private String loadFile(String htmlName) {
         StringBuilder contentBuilder = new StringBuilder();
         try {
-            BufferedReader in = new BufferedReader(new FileReader("gameFiles/"+htmlName+".html"));
-            this.htmlName = htmlName;
+
+           // BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\Focus\\IdeaProjects\\FMAssistant\\src\\main\\resources\\TestingFile.html"));
+            BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(
+                    "C:\\Users\\Focus\\IdeaProjects\\FMAssistant\\src\\main\\resources\\TestingFile.html"),
+                    "UTF-8"));
+            this.htmlName = htmlName+".html";
             String str;
             while ((str = in.readLine()) != null) {
                 contentBuilder.append(str);
@@ -54,13 +65,20 @@ public class DataLoadService implements DataLoadServiceInterface {
 
         Elements columnTitles = document.getElementsByTag("th").select("th");
 
+        if(!header.isEmpty())header.clear();
         //Populate Header with column titles in parsed file
         for (org.jsoup.nodes.Element columnTitle : columnTitles) {
             header.add(columnTitle.text());
         }
         if(this.checkOrder(template)){
-            //TODO process data
-            System.out.println("File is valid");
+
+            //Extract each row (player) from document
+            Elements rows = document.getElementsByTag("tr");
+
+            for (int r = 1; r < rows.size(); r++) {
+                Elements cells = rows.get(r).getElementsByTag("td");
+                footballerCreatorService.createFootballer(cells,fileType);
+            }
         } else {
             throw new IllegalStateException("Source file: " + htmlName + " has wrong column order");
         }
